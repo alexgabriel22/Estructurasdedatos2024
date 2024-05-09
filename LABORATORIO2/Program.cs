ab85e29fc10fa5b7ad6fe90df5a928706d96b179
alexgabriel22 <168763888+alexgabriel22@users.noreply.github.com>
2024-05-09 11:03:30 -0500

    codigo lab2

---
+++ LABORATORIO2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese un número entero positivo para sumar los primeros N números:");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        int suma = 0;
+
+        for (int i = 1; i <= N; i++)
+        {
+            suma += i;
+        }
+
+        Console.WriteLine("La suma de los primeros " + N + " números es: " + suma);
+    }
+}
