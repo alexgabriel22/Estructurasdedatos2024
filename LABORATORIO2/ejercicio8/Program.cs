836eea0caf0fbfbc5fce14911f0acca729ffcbc1
alexgabriel22 <168763888+alexgabriel22@users.noreply.github.com>
2024-05-09 13:03:37 -0500

    ejercicio8 labo2

---
+++ LABORATORIO2/ejercicio8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Definir las dimensiones de la matriz de matrices
+        int filas = 3;
+        int columnas = 2;
+
+        // Crear la matriz de matrices
+        int[][] matrizDeMatrices = new int[filas][];
+
+        // Llenar la matriz con matrices individuales
+        for (int i = 0; i < filas; i++)
+        {
+            matrizDeMatrices[i] = new int[columnas];
+            for (int j = 0; j < columnas; j++)
+            {
+                // Cada elemento de la matriz es una matriz de tamaño fijo
+                matrizDeMatrices[i][j] = i + j; // Aquí puedes inicializar cada matriz como desees
+            }
+        }
+
+        // Imprimir la matriz de matrices
+        Console.WriteLine("Matriz de matrices:");
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                Console.Write(matrizDeMatrices[i][j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
