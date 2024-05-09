2547973929f712c191d935e4cdd37d06ce5380f2
alexgabriel22 <168763888+alexgabriel22@users.noreply.github.com>
2024-05-09 12:48:09 -0500

    ejercicio6 labo2

---
+++ LABORATORIO2/ejercicio6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Definir las dimensiones de la matriz
+        int filas = 3;
+        int columnas = 4;
+
+        // Crear la matriz de números reales
+        double[,] matriz = new double[filas, columnas];
+
+        // Llenar la matriz con valores aleatorios
+        Random rnd = new Random();
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                matriz[i, j] = rnd.NextDouble() * 100; // Genera valores aleatorios entre 0 y 100
+            }
+        }
+
+        // Imprimir la matriz
+        Console.WriteLine("Matriz de números reales:");
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                Console.Write(matriz[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
