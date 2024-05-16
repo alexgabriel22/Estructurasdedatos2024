4144b762b0d93ff3040c22e7e74a0fda222e8860
alexgabriel22 <168763888+alexgabriel22@users.noreply.github.com>
2024-05-16 10:58:38 -0500

    ejercicio 12

---
+++ laboratorio 8 listas/ejercicio 12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static HashSet<int> SortedNumbersDescending(HashSet<int> numbers)
+    {
+        var sortedNumbers = numbers.OrderByDescending(num => num);
+        return new HashSet<int>(sortedNumbers);
+    }
+
+    static void Main(string[] args)
+    {
+        HashSet<int> numbers = new HashSet<int> { 5, 3, 8, 1, 9, 2, 7 };
+
+        HashSet<int> sortedSet = SortedNumbersDescending(numbers);
+
+        Console.WriteLine("Sorted numbers in descending order:");
+        foreach (int num in sortedSet)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
