783c2885d7c174c896439f8d953dda009432ff75
alexgabriel22 <168763888+alexgabriel22@users.noreply.github.com>
2024-05-16 11:19:48 -0500

    ejercicio 17

---
+++ laboratorio 8 listas/ejercicio 17/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<string> WordsWithLength(List<string> words, int length)
+    {
+        List<string> result = new List<string>();
+
+        foreach (string word in words)
+        {
+            if (word.Length == length)
+            {
+                result.Add(word);
+            }
+        }
+
+        result.Sort();
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        List<string> words = new List<string> { "apple", "banana", "pear", "orange", "kiwi", "grape" };
+        int targetLength = 5;
+        List<string> wordsWithLength = WordsWithLength(words, targetLength);
+
+        Console.WriteLine($"Palabras de longitud {targetLength} y ordenadas:");
+        foreach (string word in wordsWithLength)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
