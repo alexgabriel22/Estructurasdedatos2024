1e758f3e9dd503d58ba0bfa1ea7f9c92a23c868d
alexgabriel22 <168763888+alexgabriel22@users.noreply.github.com>
2024-05-16 11:07:32 -0500

    ejercicio 13

---
+++ laboratorio 8 listas/ejercicio 13/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static HashSet<int> FindDuplicates(int[] numbers)
+    {
+        HashSet<int> uniqueNumbers = new HashSet<int>();
+        HashSet<int> duplicates = new HashSet<int>();
+
+        foreach (int num in numbers)
+        {
+            // Si el número ya está en uniqueNumbers, es un duplicado
+            if (!uniqueNumbers.Add(num))
+            {
+                duplicates.Add(num);
+            }
+        }
+
+        return duplicates;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10, 10 };
+        HashSet<int> duplicates = FindDuplicates(numbers);
+
+        Console.WriteLine("Números duplicados:");
+        foreach (int num in duplicates)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
+
