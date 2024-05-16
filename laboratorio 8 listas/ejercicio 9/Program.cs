da5438cab4d050bf8ab5a1c411263fa11a996bee
alexgabriel22 <168763888+alexgabriel22@users.noreply.github.com>
2024-05-16 10:53:21 -0500

    ejercicio 9

---
+++ laboratorio 8 listas/ejercicio 9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<string> WordsWithLength(HashSet<string> words, int length)
+    {
+        List<string> wordsOfLength = new List<string>();
+
+        foreach (string word in words)
+        {
+            if (word.Length == length)
+            {
+                wordsOfLength.Add(word);
+            }
+        }
+
+        return wordsOfLength;
+    }
+
+    static void Main(string[] args)
+    {
+        HashSet<string> words = new HashSet<string> { "apple", "banana", "orange", "pear", "grape" };
+
+        int targetLength = 5;
+        List<string> wordsOfLength = WordsWithLength(words, targetLength);
+
+        Console.WriteLine($"Words with length {targetLength}:");
+        foreach (string word in wordsOfLength)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
+
