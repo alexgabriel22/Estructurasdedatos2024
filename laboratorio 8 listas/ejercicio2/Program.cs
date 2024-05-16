d270e0bfc8bef04227c25db1d01073cf47fe34db
alexgabriel22 <168763888+alexgabriel22@users.noreply.github.com>
2024-05-16 10:35:57 -0500

    ejercicio 2

---
+++ laboratorio 8 listas/ejercicio2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ejemplo de uso
+        string[] palabras = { "gato", "perro", "girasol", "elefante", "zorro", "manzana" };
+        char letraInicio = 'g';
+        HashSet<string> palabrasConLetra = ObtenerPalabrasConLetra(palabras, letraInicio);
+
+        Console.WriteLine($"Palabras que comienzan con '{letraInicio}':");
+        foreach (string palabra in palabrasConLetra)
+        {
+            Console.WriteLine(palabra);
+        }
+    }
+
+    static HashSet<string> ObtenerPalabrasConLetra(string[] palabras, char letra)
+    {
+        HashSet<string> palabrasConLetra = new HashSet<string>();
+
+        foreach (string palabra in palabras)
+        {
+            if (palabra.StartsWith(letra.ToString(), StringComparison.OrdinalIgnoreCase))
+            {
+                palabrasConLetra.Add(palabra);
+            }
+        }
+
+        return palabrasConLetra;
+    }
+}
