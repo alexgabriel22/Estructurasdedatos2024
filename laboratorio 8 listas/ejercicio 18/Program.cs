1808312f99d8ccb056fb9b1a39b7cb6a83d4ddb4
alexgabriel22 <168763888+alexgabriel22@users.noreply.github.com>
2024-05-16 11:22:40 -0500

    ejercicio 18

---
+++ laboratorio 8 listas/ejercicio 18/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static List<string> WordsWithLetter(List<string> words, char letter)
+    {
+        List<string> result = new List<string>();
+
+        foreach (string word in words)
+        {
+            if (word.Contains(letter))
+            {
+                result.Add(word);
+            }
+        }
+
+        result = result.OrderByDescending(w => w).ToList();
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        List<string> words = new List<string> { "apple", "banana", "pear", "orange", "kiwi", "grape" };
+        char targetLetter = 'a';
+        List<string> wordsWithLetter = WordsWithLetter(words, targetLetter);
+
+        Console.WriteLine($"Palabras que contienen la letra '{targetLetter}' y ordenadas de mayor a menor:");
+        foreach (string word in wordsWithLetter)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
