dac989f10284f8560e7ebb826f6b93f598a912ce
alexgabriel22 <168763888+alexgabriel22@users.noreply.github.com>
2024-05-16 10:41:34 -0500

    ejercicio 4

---
+++ laboratorio 8 listas/ejecicio4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ejemplo de uso
+        HashSet<int> conjunto1 = new HashSet<int> { 1, 2, 3, 4, 5 };
+        HashSet<int> conjunto2 = new HashSet<int> { 3, 4, 5, 6, 7 };
+
+        HashSet<int> numerosComunes = ObtenerNumerosComunes(conjunto1, conjunto2);
+
+        Console.WriteLine("Números comunes:");
+        foreach (int numero in numerosComunes)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+
+    static HashSet<int> ObtenerNumerosComunes(HashSet<int> conjunto1, HashSet<int> conjunto2)
+    {
+        HashSet<int> numerosComunes = new HashSet<int>(conjunto1);
+        numerosComunes.IntersectWith(conjunto2);
+        return numerosComunes;
+    }
+}
