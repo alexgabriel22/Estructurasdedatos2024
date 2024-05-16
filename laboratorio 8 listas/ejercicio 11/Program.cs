b0b66b504445317dde81fd9e58f8849c7d605e69
alexgabriel22 <168763888+alexgabriel22@users.noreply.github.com>
2024-05-16 10:57:06 -0500

    ejercicio 11

---
+++ laboratorio 8 listas/ejercicio 11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static HashSet<int> SortedNumbers(HashSet<int> numbers)
+    {
+        List<int> sortedList = numbers.ToList();
+        sortedList.Sort();
+
+        return new HashSet<int>(sortedList);
+    }
+
+    static void Main(string[] args)
+    {
+        HashSet<int> numbers = new HashSet<int> { 5, 3, 8, 1, 9, 2, 7 };
+
+        HashSet<int> sortedSet = SortedNumbers(numbers);
+
+        Console.WriteLine("Sorted numbers:");
+        foreach (int num in sortedSet)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
+
