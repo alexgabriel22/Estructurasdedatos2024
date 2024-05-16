d0cc9a602836e27717dc6ab51a7c3a82c77386d9
alexgabriel22 <168763888+alexgabriel22@users.noreply.github.com>
2024-05-16 10:55:08 -0500

    ejercicio 10

---
+++ laboratorio 8 listas/ejercicio 10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<string> WordsContainingLetter(HashSet<string> words, char letter)
+    {
+        List<string> wordsWithLetter = new List<string>();
+
+        foreach (string word in words)
+        {
+            if (word.Contains(letter))
+            {
+                wordsWithLetter.Add(word);
+            }
+        }
+
+        return wordsWithLetter;
+    }
+
+    static void Main(string[] args)
+    {
+        HashSet<string> words = new HashSet<string> { "apple", "banana", "orange", "pear", "grape" };
+
+        char targetLetter = 'a';
+        List<string> wordsWithLetter = WordsContainingLetter(words, targetLetter);
+
+        Console.WriteLine($"Words containing the letter '{targetLetter}':");
+        foreach (string word in wordsWithLetter)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
